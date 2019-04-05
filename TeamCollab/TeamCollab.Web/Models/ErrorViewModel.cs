9427a222f161ea0cd31686624543116b897bb58b
GenchoBG <genchobg11@gmail.com>
2019-04-05 22:19:49 +0300

    Added initial solution

---
+++ TeamCollab/TeamCollab.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TeamCollab.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
