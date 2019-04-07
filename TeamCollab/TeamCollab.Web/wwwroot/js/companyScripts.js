45aa53d628abd55c848886412ac2542f5562667b
Owliie <sladurcheto5232@abv.bg>
2019-04-08 02:32:45 +0300

    Saved js file...... ooooooooops

--- TeamCollab/TeamCollab.Web/wwwroot/js/companyScripts.js
+++ TeamCollab/TeamCollab.Web/wwwroot/js/companyScripts.js
@@ -10,13 +10,14 @@
                         .append($(`<td>`).text(user.userName))
                         .append($(`<td>`).text(user.email))
                         .append($('<td>')
-                            .append($(`<button class="btn btn-outline-info">`).text("Promote")
+                            .append($(`<button class="btn btn-outline-info" id="btn-${user.id}">`).text("Promote")
                                 .on("click",
                                     function () {
                                         $.ajax({
                                             type: "GET",
                                             url: `/Company/Promote/${user.id}`,
                                             success: function () {
+                                                $(`#btn-${user.id}`).addClass("disabled");
                                                 $(`#${user.id}`).fadeOut("slow");
                                             }
                                         });
