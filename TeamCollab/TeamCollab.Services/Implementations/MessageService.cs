ff084f58f825611c58e678d91407a6eda378e273
Owliie <sladurcheto5232@abv.bg>
2019-05-03 00:10:57 +0300

    Added message services

---
+++ TeamCollab/TeamCollab.Services/Implementations/MessageService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamCollab.Data;
+using TeamCollab.Data.Models;
+using TeamCollab.Services.Interfaces;
+
+namespace TeamCollab.Services.Implementations
+{
+    public class MessageService : IMessageService
+    {
+        private readonly TeamCollabDbContext db;
+        private const int Count = 50;
+
+        public MessageService(TeamCollabDbContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task Add(string content, string senderId, int projectId)
+        {
+            await this.db.Messages.AddAsync(new Message()
+            {
+                Content = content,
+                Created = DateTime.Now,
+                UserId = senderId,
+                ProjectId = projectId
+            });
+
+            await this.db.SaveChangesAsync();
+        }
+
+        public async Task Destroy(int id)
+        {
+            this.db.Messages.Remove(await this.db.Messages.FindAsync(id));
+
+            await this.db.SaveChangesAsync();
+        }
+
+        public IQueryable<Message> GetLast(int lastLoadedMessageId)
+        {
+            return this.db.Messages.Where(m => m.Id > lastLoadedMessageId).Take(Count);
+        }
+
+        public IQueryable<Message> GetLast()
+        {
+            return this.db.Messages.TakeLast(Count);
+        }
+    }
+}
---
+++ TeamCollab/TeamCollab.Services/Interfaces/MessageService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamCollab.Data.Models;
+
+namespace TeamCollab.Services.Interfaces
+{
+    public interface IMessageService
+    {
+        Task Add(string content, string senderId, int projectId);
+        Task Destroy(int id);
+        IQueryable<Message> GetLast(int lastLoadedMessageId);
+        IQueryable<Message> GetLast();
+    }
+}
