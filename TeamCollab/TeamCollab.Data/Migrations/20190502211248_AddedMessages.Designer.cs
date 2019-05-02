0eeff29489cfd0c845b47f850bb71f3b67e99f9d
Owliie <sladurcheto5232@abv.bg>
2019-05-03 00:15:38 +0300

    Message db migrations added

--- TeamCollab/TeamCollab.Data/Migrations/20190502211248_AddedMessages.Designer.cs
+++ TeamCollab/TeamCollab.Data/Migrations/20190502211248_AddedMessages.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TeamCollab.Data;
 
 namespace TeamCollab.Data.Migrations
 {
     [DbContext(typeof(TeamCollabDbContext))]
-    partial class TeamCollabDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190502211248_AddedMessages")]
+    partial class AddedMessages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TeamCollab/TeamCollab.Data/Migrations/20190502211248_AddedMessages.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TeamCollab.Data.Migrations
+{
+    public partial class AddedMessages : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Heading",
+                table: "Projects",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 25);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Projects",
+                maxLength: 5000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 500);
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Content = table.Column<string>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    UserId = table.Column<string>(nullable: false),
+                    ProjectId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Messages_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Messages_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_ProjectId",
+                table: "Messages",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_UserId",
+                table: "Messages",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Heading",
+                table: "Projects",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Projects",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 5000);
+        }
+    }
+}
